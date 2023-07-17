edd3576cf9da9148286b12aafe3b112beedcc005
Jimmy Jurado <jurado140@gmail.com>
2023-07-17 15:09:00 -0400

    clean up-same code as main.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
- 
-    # movie_id = @user.parties.first.movie_id
-    # @movie = SearchFacade.new(params[movie_id]).movies.first
-    # @party = @user.parties.first
   end
 
   def new
